301897e544115ae153e762f7973fb8bdf1cbc2f8
okok111 <1for.rails@gmail.com>
2022-03-03 16:15:41 +0900

    ページ遷移

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,8 @@
 <template>
   <h1>Home</h1>
-  <div>ここにそれぞれのアプリに行けるリンクを設置する</div>
+  <div>
+    <router-link to="/memo">メモアプリ</router-link> |
+    <router-link to="/palette">パレット</router-link> |
+    <router-link to="/quiz">クイズアプリ</router-link>
+  </div>
 </template>
