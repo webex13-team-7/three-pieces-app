e1753bba62146dd3786bdbdfb4a5d12df5eb3874
okok111 <1for.rails@gmail.com>
2022-03-04 16:07:29 +0900

    フッター完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,49 @@
 <template>
   <h1>Home</h1>
-  <div>
-    <router-link to="/memo">メモアプリ</router-link> |
-    <router-link to="/palette">パレット</router-link> |
-    <router-link to="/quiz">クイズアプリ</router-link>
+  <div class="home">
+    <router-link to="/memo" class="nav__link">
+      <div class="word">メモアプリ</div>
+    </router-link>
+    <router-link to="/palette" class="nav__link1">
+      <div class="word">パレット</div>
+    </router-link>
+    <router-link to="/quiz" class="nav__link">
+      <div class="word">クイズ</div>
+    </router-link>
   </div>
 </template>
+
+<style scoped>
+.home {
+  width: 1000px;
+  margin: 0 auto;
+  display: flex;
+  text-align: center;
+  font-size: 50px;
+}
+
+.nav__link {
+  width: 300px;
+  height: 200px;
+  background-color: #41b883;
+  color: #2c3e50;
+  text-decoration: none;
+  border-radius: 20px;
+  box-shadow: 2px 3px 2px 1px;
+}
+
+.nav__link1 {
+  width: 300px;
+  height: 200px;
+  background-color: #42b983;
+  color: #2c3e50;
+  margin: 0 20px;
+  vertical-align: middle;
+  text-decoration: none;
+  border-radius: 20px;
+  box-shadow: 2px 3px 2px 1px;
+}
+.word {
+  margin-top: 15%;
+}
+</style>
