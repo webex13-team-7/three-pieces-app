e1b1f7cd7e33613a7d37c2e6079d535707863709
okok111 <1for.rails@gmail.com>
2022-03-05 21:06:34 +0900

    ボタン完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <h1>Home</h1>
   <div class="home">
     <router-link to="/memo" class="nav__link">
-      <div class="word">メモアプリ</div>
+      <div class="word">メモ</div>
     </router-link>
     <router-link to="/palette" class="nav__link1">
       <div class="word">パレット</div>
@@ -29,9 +29,14 @@
   color: #2c3e50;
   text-decoration: none;
   border-radius: 20px;
-  box-shadow: 2px 3px 2px 1px;
+  box-shadow: 2px 3px 2px 1px #2c3e50;
 }
 
+.nav__link:hover {
+  width: 290px;
+  height: 190px;
+  color: red;
+}
 .nav__link1 {
   width: 300px;
   height: 200px;
@@ -41,7 +46,13 @@
   vertical-align: middle;
   text-decoration: none;
   border-radius: 20px;
-  box-shadow: 2px 3px 2px 1px;
+  box-shadow: 2px 3px 2px 1px #2c3e50;
+}
+
+.nav__link1:hover {
+  width: 290px;
+  height: 190px;
+  color: red;
 }
 .word {
   margin-top: 15%;
